2be72b6e39956c770744d54b9b353ed670263b7e
daleoooo <daleoooo.gmail.com>
2019-02-25 19:17:41 +0800

    Initial Commit

---
+++ web/[load-resource]-only-html/webpack.conf.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './path/to/my/entry/file.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-first-webpack.bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.txt$/, use: 'raw-loader' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ]
+};
